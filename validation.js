ebd30898ec394059a84e1ccfce2c4ade026c498e
VR <vitrayev@gmail.com>
2021-07-14 17:01:46 -0400

    added regular expression for email addresses

--- validation.js
+++ validation.js
@@ -7,8 +7,8 @@ const patterns = {
     telephone: /^\d{10}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
-    slug: /^[a-z\d-]{8,20}$/
-
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 //validation function
